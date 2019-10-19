fbff97d8207f6df03849f5c581970d5792841920
Ivy Jackson <ivyjs@hendrix.edu>
2019-10-19 12:59:46 -0500

    Added Log in and Clock in Pages

---
+++ Pages/Login.cshtml.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace CityOfHopeVolunteerTracking.Pages
+{
+    public class LoginModel : PageModel
+    {
+        private readonly ILogger<LoginModel> _logger;
+
+        public LoginModel(ILogger<LoginModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
