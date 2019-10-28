6379a63a1529b2d7a6ac606f9d7e372d74ca4cf5
Dan Turner <turner@conwaycorp.net>
2019-10-28 12:02:05 -0500

    Revert "Follwed movie demo steps to create a volunteer table with full CRUD"
    This reverts commit b8180397466481e233762b843ff027e4bb843cff.

--- Startup.cs
+++ Startup.cs
@@ -8,8 +8,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore;
-using CityOfHopeVolunteerTracking.Data;
 
 namespace CityOfHopeVolunteerTracking
 {
@@ -26,9 +24,6 @@ namespace CityOfHopeVolunteerTracking
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-
-            services.AddDbContext<CityOfHopeVolunteerTrackingContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("CityOfHopeVolunteerTrackingContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
