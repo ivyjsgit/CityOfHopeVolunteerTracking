8574a745974bc603432f25c3a0edd5d2104cde0a
Ivy Jackson <ivyjs@hendrix.edu>
2019-10-29 13:56:17 -0500

    Added temporarily link to admin page and updated logo size

--- Models/Volunteer.cs
+++ Models/Volunteer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace CityOfHopeVolunteerTracking.Models
     public class Volunteer
     {
         public int ID { get; set; }
+
         public string First { get; set; }
         public string Last { get; set; }
         public string Email { get; set; }
