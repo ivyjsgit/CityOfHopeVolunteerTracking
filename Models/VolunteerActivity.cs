71f67fa17b6b55ad1b8d6553c730754b9f687760
Dan <turner@conwaycorp.net>
2019-11-03 23:03:17 -0600

    Add Volunteer activity class
    Add table to volunteer detail page

---
+++ Models/VolunteerActivity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityOfHopeVolunteerTracking.Models
+{
+    public class VolunteerActivity
+    {
+        public int actvityID { get; set; }
+        public string initiative { get; set; }
+        public string userName { get; set; }
+        public DateTime startTime { get; set; }
+        public DateTime endTime { get; set; }
+        public int elapsedTime { get; set; }
+    }
+}
