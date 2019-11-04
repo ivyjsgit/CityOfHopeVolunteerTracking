0bd5f2e7a0e764866789ad59d16b07ff8a435eaa
Vi Nguyen <nguyenvv@hendrix.edu>
2019-11-04 16:10:31 -0600

    Vi's changes to work with clock in functionality

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -10,6 +10,29 @@ namespace CityOfHopeVolunteerTracking.Pages
 {
     public class IndexModel : PageModel
     {
+        Boolean clockin = false;
+        DateTime clockin_time = DateTime.Now;
+        DateTime clockout_time = DateTime.Now;
+        int temp_user_id = 1;
+
+        protected void button1_click(object sender, EventArgs e)
+        {
+            if (temp_user_id == 1) {
+                if (clockin == false)
+                {
+                    this.clockin = true;
+                    this.clockin_time = DateTime.Now;
+
+
+                }
+                else if (clockin == true)
+                {
+                    this.clockin = false;
+                    this.clockout_time = DateTime.Now;
+                }
+            }
+        }
+
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
