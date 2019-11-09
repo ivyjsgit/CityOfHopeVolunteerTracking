7b45dfc885cfc946de3807dca64a49266a150bd5
Dan <turner@conwaycorp.net>
2019-11-08 23:03:35 -0600

    Added 4th button that gets information from the database

--- Pages/ButtonTest.cshtml.cs
+++ Pages/ButtonTest.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CityOfHopeVolunteerTracking.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -10,6 +11,15 @@ namespace CityOfHopeVolunteerTracking.Pages
     public class ButtonTestModel : PageModel
     {
         public string Message { get; set; }
+
+    private readonly CityOfHopeVolunteerTracking.Data.COHODatabaseContext _context;
+
+        public ButtonTestModel(CityOfHopeVolunteerTracking.Data.COHODatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public Volunteer Volunteer { get; set; }
         public void OnGet()
         {
             Message = "OnGet used";
@@ -20,10 +30,28 @@ namespace CityOfHopeVolunteerTracking.Pages
             Message = "OnPost used";
         }
 
-        public void OnPostView(int id)
+        public void OnPostView()
         {
             Message = "You called the OnPostView Method";
         }
 
+        public void OnPostRecord(int? id)
+        {
+            if (id == null)
+            {
+                Message = "Record not Found";
+            }
+
+            Volunteer = _context.Volunteer.FirstOrDefault(m => m.ID == id);
+
+            if (Volunteer == null)
+            {
+                Message = "Record not Found";
+            }
+            else
+            {
+                Message = Volunteer.First + " " + Volunteer.Last;
+            }
+        }
     }
 }
