ca5b46491466a3fd3ee4b11e8131430ffef37c68
Andrew Eveld <andreweveld99@gmail.com>
2019-11-29 22:31:05 -0600

    fixed value of hours querying on excel creation
    admin index

--- Pages/AdminIndex.cshtml.cs
+++ Pages/AdminIndex.cshtml.cs
@@ -66,17 +66,22 @@ namespace CoHO.Pages
             //Adding text to cells.
             for (int i = 1; i < numInitiatives + 1; i++)
             {
-                worksheet.Range[i + 1, 1].Text = initiatives.Single(m => m.InitiativeID == i).Description;
+                worksheet.Range[2 * i, 1].Text = initiatives.Single(m => m.InitiativeID == i).Description + "(non-staff) hours";
+                worksheet.Range[2 * i + 1, 1].Text = initiatives.Single(m => m.InitiativeID == i).Description + "(non-staff) value";
                 for (int j = 1; j < 13; j++)
                 {
                     double hours = 0;
+                    double value = 0;
                     var activities = volunteerActivities.Where(m => m.StartTime.Month == j && m.InitiativeId == i);
                     foreach (var activity in activities)
                     {
-                        hours += activity.ElapsedTime.Hours + activity.ElapsedTime.Minutes / 60;
+                        double time = activity.ElapsedTime.Hours + activity.ElapsedTime.Minutes / 60;
+                        hours += time;
+                        value += time * valuesOfHours.OrderBy(m => m.EffectiveDate).Last(m => m.EffectiveDate <= activity.StartTime).Value;
                     }
                     initiativeMonth[i - 1, j - 1] = hours;
-                    worksheet.Range[i + 1, j + 1].Number = hours;
+                    worksheet.Range[2 * i, j + 1].Number = hours;
+                    worksheet.Range[2 * i + 1, j + 1].Number = value;
                 }
                 
                 
@@ -87,57 +92,9 @@ namespace CoHO.Pages
                 worksheet.Range[1, i + 1].Text = months[i - 1];
             }
 
-            worksheet.Range["A16"].Text = "January";
-            worksheet.Range["A17"].Text = "Feburary";
-            worksheet.Range["A18"].Text = "March";
-            worksheet.Range["A19"].Text = "April";
-            worksheet.Range["A20"].Text = "May";
-            worksheet.Range["A21"].Text = "June";
-            worksheet.Range["A22"].Text = "July";
-            worksheet.Range["A23"].Text = "August";
-            worksheet.Range["A24"].Text = "September";
-            worksheet.Range["A25"].Text = "October";
-            worksheet.Range["A26"].Text = "November";
-            worksheet.Range["A27"].Text = "December";
-            worksheet.Range["B15"].Text = "Staff Hours";
-            worksheet.Range["C15"].Text = "Staff Value";
-            worksheet.Range["D15"].Text = "Volunteer Hours";
-            worksheet.Range["E15"].Text = "Volunteer Value";
-            worksheet.Range["F15"].Text = "Total Hours";
-            worksheet.Range["G15"].Text = "Total Value";
+          
 
-            for (int i = 1; i < 13; i++)
-            {
-                double staffHours = 0;
-                double volunteerHours = 0;
-                foreach (var volunteerActivity in volunteerActivities)
-                {
-                    Volunteer thisVolunteer = volunteers.Single(m => m.VolunteerID == volunteerActivity.VolunteerId);
-                    DateTime date = volunteerActivity.StartTime;
-                    Console.Write(volunteerActivity.StartTime);
-                    if (DateTime.Now.Year == date.Year && date.Month == i)
-                    {
-                        if (thisVolunteer.VolunteerTypeID == 1)
-                        {
-                            volunteerHours += volunteerActivity.ElapsedTime.Minutes / 60 + volunteerActivity.ElapsedTime.Hours;
-                        }
-                        else
-                        {
-                            staffHours += volunteerActivity.ElapsedTime.Minutes / 60 + volunteerActivity.ElapsedTime.Hours;
-                        }
-                    }
-                }
-                DateTime dateTime = new DateTime(DateTime.Today.Year, i, 1);
-                ValueOfHour valueOfHour = valuesOfHours.OrderBy(m => m.EffectiveDate).Last(m => m.EffectiveDate <= dateTime);
-                worksheet.Range["B" + (i + 15)].Number = staffHours;
-                worksheet.Range["C" + (i + 15)].Number = Math.Round((staffHours * valueOfHour.Value), 2);
-                worksheet.Range["D" + (i + 15)].Number = volunteerHours;
-                worksheet.Range["E" + (i + 15)].Number = Math.Round((volunteerHours * valueOfHour.Value), 2);
-                worksheet.Range["F" + (i + 15)].Number = staffHours + volunteerHours;
-                worksheet.Range["G" + (i + 15)].Number = Math.Round(((staffHours + volunteerHours) * valueOfHour.Value), 2);
-
-
-            }
+            
 
 
 
