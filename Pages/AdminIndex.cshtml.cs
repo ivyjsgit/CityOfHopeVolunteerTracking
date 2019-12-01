47df16a98230caa52f687eb410bfad7cb89f9eb5
Andrew Eveld <andreweveld99@gmail.com>
2019-12-01 02:09:21 -0600

    fixed layout of admin excel

--- Pages/AdminIndex.cshtml.cs
+++ Pages/AdminIndex.cshtml.cs
@@ -43,7 +43,7 @@ namespace CoHO.Pages
             var valuesOfHours = _context.ValueOfHour;
             var initiatives = _context.Initiative;
 
-            //Save information to dictionary
+            //Save information to 2d array.
             int numInitiatives = initiatives.Count();
             double[,] initiativeMonth = new double[numInitiatives, 13];
 
@@ -60,28 +60,74 @@ namespace CoHO.Pages
             //Accessing first worksheet in the workbook.
             IWorksheet worksheet = workbook.Worksheets[0];
 
-
+            //Setting first column width to 25.
             worksheet.SetColumnWidth(1, 25);
 
-            //Adding text to cells.
-            for (int i = 1; i < numInitiatives + 1; i++)
+            //Setting all other column widths to 15.
+            for (int i = 2; i < 20; i++)
             {
-                worksheet.Range[2 * i, 1].Text = initiatives.Single(m => m.InitiativeID == i).Description + "(non-staff) hours";
-                worksheet.Range[2 * i + 1, 1].Text = initiatives.Single(m => m.InitiativeID == i).Description + "(non-staff) value";
+                worksheet.SetColumnWidth(i, 15);
+            }
+
+            //looping through initiatives
+            for (int i = 0; i < numInitiatives + 1; i++)
+            {
+                
+                if (i > 0)
+                {
+                    worksheet.Range[2 * (i + 1), 1].Text = initiatives.Single(m => m.InitiativeID == i).Description + "(non-staff) hours";
+                    worksheet.Range[2 * (i + 1) + 1, 1].Text = initiatives.Single(m => m.InitiativeID == i).Description + "(non-staff) value";
+                }
+                else
+                {
+                    worksheet.Range[2, 1].Text = "Staff Hours";
+                    worksheet.Range[3, 1].Text = "Staff Value";
+                }
+                // labels for the months and initiatives.
+
+                //looping through the months.
                 for (int j = 1; j < 13; j++)
                 {
+                    // Initializing the hours and values to keep track of current month's (j) total values and hours.
                     double hours = 0;
                     double value = 0;
-                    var activities = volunteerActivities.Where(m => m.StartTime.Month == j && m.InitiativeId == i);
+
+                    //querying the activities of the current month (j) and initiative id (i).
+                    System.Linq.IQueryable<CoHO.Models.VolunteerActivity> activities;
+
+                    if (i == 0)
+                    {
+                        activities = volunteerActivities.Where(m => m.StartTime.Month == j);
+                    }
+                    else
+                    {
+                        activities = volunteerActivities.Where(m => m.StartTime.Month == j && m.InitiativeId == i);
+
+                    }
+
                     foreach (var activity in activities)
                     {
+                        // time for this one activity
                         double time = activity.ElapsedTime.Hours + activity.ElapsedTime.Minutes / 60;
-                        hours += time;
-                        value += time * valuesOfHours.OrderBy(m => m.EffectiveDate).Last(m => m.EffectiveDate <= activity.StartTime).Value;
+
+                        //adding the current activities hours and values to their respective variables
+                        if (volunteers.Single(m => m.VolunteerID == activity.VolunteerId).VolunteerTypeID == 1 && i != 0)
+                        {
+                            hours += time;
+                            value += time * valuesOfHours.OrderBy(m => m.EffectiveDate).Last(m => m.EffectiveDate <= activity.StartTime).Value;
+                        }
+                        else if (volunteers.Single(m => m.VolunteerID == activity.VolunteerId).VolunteerTypeID == 2 && i == 0)
+                        {
+                            hours += time;
+                            value += time * valuesOfHours.OrderBy(m => m.EffectiveDate).Last(m => m.EffectiveDate <= activity.StartTime).Value;
+                        }
+
                     }
-                    initiativeMonth[i - 1, j - 1] = hours;
-                    worksheet.Range[2 * i, j + 1].Number = hours;
-                    worksheet.Range[2 * i + 1, j + 1].Number = value;
+                    //initiativeMonth[i - 1, j - 1] = hours;
+
+                    //adding the hours and values to the table.
+                    worksheet.Range[2 * (i + 1), j + 1].Number = Math.Round(hours, 2);
+                    worksheet.Range[2 * (i + 1) + 1, j + 1].Number = Math.Round(value, 2);
                 }
                 
                 
