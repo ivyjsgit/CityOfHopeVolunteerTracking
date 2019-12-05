dc2e47d6cd8efb861b3d340818a2ffb32b27cd59
Privateeye421 <johnjackfrey@gmail.com>
2019-12-04 22:39:26 -0600

    You can download blank Word documents

--- Pages/VolunteerReports.cshtml.cs
+++ Pages/VolunteerReports.cshtml.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System.IO;
 
 namespace CoHO.Pages
 {
@@ -12,6 +15,22 @@ namespace CoHO.Pages
         public void OnGet()
         {
 
+        }
+
+        public FileStreamResult OnPostView() {
+            Console.WriteLine('H');
+            WordDocument report = new WordDocument();
+            report.EnsureMinimal();
+            IWSection section = report.AddSection();
+            IWParagraph name = section.AddParagraph();
+            name.ParagraphFormat.HorizontalAlignment = Syncfusion.DocIO.DLS.HorizontalAlignment.Center;
+            IWTextRange nameText = name.AppendText("Jack Frey");
+            nameText.CharacterFormat.FontName = "Times New Roman";
+            nameText.CharacterFormat.FontSize = 14;
+            MemoryStream stream = new MemoryStream();
+            stream.Position = 0;
+            report.Close();
+            return File(stream, "application/msword", "JackFreyReport.docx");
         }
     }
 }
