abe89eac00a867a7873d081c0db45f88d65a971b
Privateeye421 <johnjackfrey@gmail.com>
2019-12-05 12:56:16 -0600

    You can download A Word Document with my name in it

--- Pages/VolunteerReports.cshtml.cs
+++ Pages/VolunteerReports.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Syncfusion.DocIO;
 using Syncfusion.DocIO.DLS;
+using Syncfusion.XlsIO;
 using System.IO;
 
 namespace CoHO.Pages
@@ -20,16 +21,18 @@ namespace CoHO.Pages
         public FileStreamResult OnPostView() {
             Console.WriteLine('H');
             WordDocument report = new WordDocument();
-            report.EnsureMinimal();
             IWSection section = report.AddSection();
             IWParagraph name = section.AddParagraph();
             name.ParagraphFormat.HorizontalAlignment = Syncfusion.DocIO.DLS.HorizontalAlignment.Center;
+            IWTable info = section.AddTable();
+            info.ResetCells(2, 2);
             IWTextRange nameText = name.AppendText("Jack Frey");
             nameText.CharacterFormat.FontName = "Times New Roman";
             nameText.CharacterFormat.FontSize = 14;
             MemoryStream stream = new MemoryStream();
-            stream.Position = 0;
+            report.Save(stream, FormatType.Docx);
             report.Close();
+            stream.Position = 0;
             return File(stream, "application/msword", "JackFreyReport.docx");
         }
     }
