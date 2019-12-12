e655a05593969b7f4acba932cd6ba8af6a288200
Dan <turner@conwaycorp.net>
2019-12-11 23:03:33 -0600

    Added buttons to update all tables to admin index page
    Added layouts to password rest pages

--- Pages/Initiatives/Index.cshtml.cs
+++ Pages/Initiatives/Index.cshtml.cs
@@ -23,7 +23,9 @@ namespace CoHO.Pages.Initiatives
 
         public async Task OnGetAsync()
         {
-            Initiative = await _context.Initiative.ToListAsync();
+            IQueryable<Initiative> InitiativeIQueryable = from i in _context.Initiative select i;
+            InitiativeIQueryable = InitiativeIQueryable.OrderBy(i => i.Description);
+            Initiative = await InitiativeIQueryable.ToListAsync();
         }
     }
 }
