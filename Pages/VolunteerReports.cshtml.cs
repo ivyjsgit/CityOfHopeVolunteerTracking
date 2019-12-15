c0fc831bb051bf44e421d0403d4bc4512faeb6ff
Vi Nguyen <nguyenvv@hendrix.edu>
2019-12-15 15:47:39 -0600

    adding $ sign

--- Pages/VolunteerReports.cshtml.cs
+++ Pages/VolunteerReports.cshtml.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using 
+using Microsoft.AspNetCore.Mvc.RazorPages; 
 using Syncfusion.DocIO;
 using Syncfusion.DocIO.DLS;
 using Syncfusion.XlsIO;
